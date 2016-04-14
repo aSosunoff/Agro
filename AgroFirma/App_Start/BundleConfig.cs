017ab4dcff5676a33bad6a8d3db1658620d8d9f2
asosunoff <al.sosunoff@gmail.com>
2016-04-14 20:33:40 +0400

    Clean project

--- AgroFirma/App_Start/BundleConfig.cs
+++ AgroFirma/App_Start/BundleConfig.cs
@@ -23,7 +23,9 @@ namespace AgroFirma
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.css",
+                "~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
@@ -38,6 +40,10 @@ namespace AgroFirma
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
         }
     }
 }
