25b56e8f104794281e506226bf99be32dbe5c3c2
asosunoff <al.sosunoff@gmail.com>
2016-04-17 12:36:45 +0300

    added file

---
+++ Model/Engine/Service/Logic/RContractService.cs
@@ -0,0 +1,12 @@
+﻿using Model.Engine.Repository.Interface;
+using Model.Engine.Service.Interface;
+
+namespace Model.Engine.Service.Logic
+{
+    public class RContractService : BaseService<IRContractRepository>, IRContractService
+    {
+        public RContractService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
