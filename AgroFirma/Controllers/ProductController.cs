15e33ccae4bafea87819cda0632c294de2f94d1e
asosunoff <al.sosunoff@gmail.com>
2016-04-17 10:41:50 +0300

    Add Entity Model MySQL.

--- AgroFirma/Controllers/ProductController.cs
+++ AgroFirma/Controllers/ProductController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model;
 
 namespace AgroFirma.Controllers
 {
@@ -13,6 +15,11 @@ namespace AgroFirma.Controllers
 
         public ActionResult Index()
         {
+
+            Class1 s = new Class1();
+
+            s.Add();
+
             return View();
         }
 
---
+++ Model/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Class1
+    {
+        public void Add()
+        {
+            agro_mydbEntities d = new agro_mydbEntities();
+
+            rnews n = new rnews()
+            {
+                DATE = DateTime.Now,
+                IMAGE_PATH = "asdas",
+                NAME = "sadfgg",
+                TEXT = "asddasd"
+            };
+
+            d.Entry(n).State = EntityState.Added;
+
+            d.SaveChanges();
+        }
+    }
+}
