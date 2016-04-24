82934f7892312e1a514b14f1100435e14beabe5a
asosunoff <al.sosunoff@gmail.com>
2016-04-24 11:56:06 +0300

    Fix Bascet List (Bootstrap, CSS)

---
+++ AgroFirma/Controllers/BasketController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AgroFirma.Component;
+using Model.Engine.Service;
+using Model.Engine.Service.Interface;
+
+namespace AgroFirma.Controllers
+{
+    public class BasketController : ControllerInitializer
+    {
+        public BasketController(IServiceLayer serviceLayer) : base(serviceLayer){}
+
+        public ActionResult List()
+        {//TODO: Необходимо привязаться к определённому пользователю. Сейчас будут выводиться все товары из корзины
+
+            return View(_serviceLayer.Get<IRBasketService>()._Repository.GetAllList().ToList());
+        }
+
+        
+    }
+}
---
+++ Model/Partial_BASKET.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [MetadataType(typeof(basket))]
+    public partial class rbasket
+    {
+    }
+
+    public class basket
+    {
+        [Display(Name = "Колличество в корзине")]
+        public int QANTITY { get; set; }
+        [Display(Name = "Дата добавления в корзину")]
+        public System.DateTime DATE_ADD_PRODUCT { get; set; }
+    }
+}
